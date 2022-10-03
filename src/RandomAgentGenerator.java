b95fa6df1ceca4919823f82c4e77907de633b5fc
yhuhz <julius.albert.grajo@gmail.com>
2022-10-04 00:23:25 +0800

    Commit 002: Added Documentation

--- src/RandomAgentGenerator.java
+++ src/RandomAgentGenerator.java
@@ -32,6 +32,9 @@ DOCUMENTATION
     - Shorten code using OOP
     - Create two methods: AgentCheck, AgentAdd, and Reset
     - Added functions such as All Duelists, All Controllers, All Initators, and all Sentinels
+10/04/2022
+    - Added a new button "NEW META" includes one Duelist, one Sentinel, one Controller, and two Initiators
+    - Properly renamed the button functions
 */
 
 /**
